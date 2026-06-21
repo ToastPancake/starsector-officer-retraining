bcc85f8ca63f5b64fed27711bea46a2c718bade0
ToastPancake <ToastPancake@users.noreply.github.com>
2026-06-21 16:51:02 -0400

    Refactor: Move mod files to repository root to match standard structure

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
 mkdir jars\bin
-"jdk\jdk-17.0.2\bin\javac.exe" -cp "C:\Program Files (x86)\Fractal Softworks\Starsector v0.98\starsector-core\*" -d jars\bin officer_retraining\src\officerretraining\*.java
-"jdk\jdk-17.0.2\bin\jar.exe" cf officer_retraining\jars\officer_retraining.jar -C jars\bin .
+"C:\Program Files\Java\jdk-17\bin\javac.exe" -cp "C:\Program Files (x86)\Fractal Softworks\Starsector v0.98\starsector-core\*;C:\Program Files (x86)\Fractal Softworks\Starsector v0.98\mods\LunaLib-2.0.5\jars\*" -d jars\bin src\officerretraining\*.java
+"C:\Program Files\Java\jdk-17\bin\jar.exe" cf jars\officer_retraining.jar -C jars\bin .
